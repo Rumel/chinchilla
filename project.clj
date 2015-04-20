0bd60339531ccbfe9e8d345038f97ddcd58f6623
Dalton Dick <daltondick2010@gmail.com>
2015-04-19 22:14:15 -0500

    Change up project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject chinchilla "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject chinchilla "0.0.1"
+  :description ""
+  :url ""
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
