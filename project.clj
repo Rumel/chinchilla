17e245980d2529af897b6e8cae1be33c06c8ec32
Dalton Dick <daltondick2010@gmail.com>
2015-05-10 21:59:33 -0500

    Add [armadillo "0.0.3"]

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.3"]
                  [ring/ring-defaults "0.1.4"]
-                 [ring/ring-json "0.3.1"]]
+                 [ring/ring-json "0.3.1"]
+                 [armadillo "0.0.3"]]
   :plugins [[lein-ring "0.9.3"]]
   :ring {:handler chinchilla.handler/app}
   :profiles
